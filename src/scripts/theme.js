879f71a8cf33a57a7b45b94c21bd6b7193f5a60b
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-04-17 21:39:23 -0300

    feat: add tailwind configs and npm scripts

---
+++ src/scripts/theme.js
@@ -0,0 +1,28 @@
+export default class Theme {
+    constructor(newTheme) {
+        if (newTheme === undefined) {
+            newTheme = this.theme;
+        }
+        this.theme = newTheme;
+        console.log(this.theme)
+    }
+
+    get theme() {
+        if (typeof localStorage !== "undefined" && localStorage.getItem("theme")) {
+            return localStorage.getItem("theme");
+        }
+        if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+            return "dark";
+        }
+        return "light";
+    }
+
+    set theme(newTheme) {
+        if (newTheme === "light") {
+            document.documentElement.classList.remove("dark");
+        } else {
+            document.documentElement.classList.add("dark");
+        }
+        window.localStorage.setItem("theme", newTheme);
+    }
+}
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'selector',
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'primary': '#C74634',
+				'secundary': '#232A31',
+				'secundary-light': '#3F4B56',
+				'white': '#F5F5F5',
+				'black': '#111111',
+				'light-gray': '#DDDDDD',
+				'dark-gray': '#BBBBBB'
+			}
+		},
 	},
 	plugins: [],
 }
