a46ecdeb37bd3f5a3eeabdbddc07bdda392a8ed9
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-04-18 15:08:21 -0300

    feat: prev and next in gallery

--- src/scripts/ImageGallery.js
+++ src/scripts/ImageGallery.js
@@ -1,28 +1,63 @@
 class ImageGallery {
     constructor(gallery) {
+        this.currentIndex = 0;
         this.gallery = gallery;
-        this.galleryImgs = this.gallery.querySelectorAll(".gallery__item");
+        this.galleryImgs = Array.from(this.gallery.querySelectorAll(".gallery__item"));
 
         this.modal = this.gallery.querySelector(".modal");
         this.modalImg = this.modal.querySelector("img");
+        this.prev = this.modal.querySelector(".prev");
+        this.next = this.modal.querySelector(".next");
         this.modalCloseButton = this.modal.querySelector(".close");
+        this.init();
+    }
 
-        this.galleryImgs.forEach((img) => {
-            img.addEventListener("click", this.modalOpen.bind(this, img));
-        });
-
-        this.modalCloseButton.addEventListener("click", () => this.modalClose());
-
-        this.modal.addEventListener("click", () => this.modalClose());
+    modalOpen(index) {
+        this.currentIndex = index;
+        this.modal.style.display = "flex";
+        this.modalImg.src = this.galleryImgs[index].src;
     }
 
     modalClose() {
         this.modal.style.display = "none";
     }
 
-    modalOpen(img) {
-        this.modal.style.display = "flex";
-        this.modalImg.src = img.src;
+    prevImage() {
+        if (this.currentIndex === 0) {
+            this.currentIndex = this.galleryImgs.length - 1;
+        } else {
+            this.currentIndex--;
+        }
+        this.modalOpen(this.currentIndex)
+    }
+
+    nextImage() {
+        if (this.currentIndex === this.galleryImgs.length - 1) {
+            this.currentIndex = 0;
+        } else {
+            this.currentIndex++;
+        }
+        this.modalOpen(this.currentIndex)
+    }
+
+    init() {
+        this.galleryImgs.forEach(img => {
+            img.addEventListener("click", () => {
+                this.modalOpen(this.galleryImgs.indexOf(img));
+            });
+        });
+
+        this.modalCloseButton.addEventListener("click", () => {
+            this.modalClose();
+        });
+
+        this.prev.addEventListener("click", () => {
+            this.prevImage();
+        });
+
+        this.next.addEventListener("click", () => {
+            this.nextImage();
+        });
     }
 }
 
