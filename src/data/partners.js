a16d05b4259bd0b64d912626fb9e4b6c765717da
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-07-21 16:59:13 -0300

    feat: rm sponsors

--- src/data/partners.js
+++ src/data/partners.js
@@ -1,34 +1,5 @@
 const sponsors = [
-    {
-        name: 'APETI',
-        logo: './imgs/partners/apeti.webp',
-        url: 'https://www.apeti.org.br',
-        invert: false
-    },
-    {
-        name: 'Shift',
-        logo: './imgs/partners/shift.webp',
-        url: 'https://shift.com.br/',
-        invert: true
-    },
-    {
-        name: 'Field Control',
-        logo: './imgs/partners/field-control.webp',
-        url: 'https://fieldcontrol.com.br/',
-        invert: false
-    },
-    {
-        name: 'Fiorilli Software',
-        logo: './imgs/partners/fiorilli-software.webp',
-        url: 'https://fiorilli.com.br/',
-        invert: true
-    },
-    {
-        name: 'TS Plus',
-        logo: './imgs/partners/ts-plus.webp',
-        url: 'https://www.tsplusbrasil.com.br/',
-        invert: false
-    },
+
 ]
 
 const support = [
