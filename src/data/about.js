03e86ee7fdfcce8d43ef3da07c25d467ee539bc4
Fabricio Tiago Arantes <88052209+Ujs74wiop6@users.noreply.github.com>
2025-06-26 19:00:49 -0300

    Update location (#15)

--- src/data/about.js
+++ src/data/about.js
@@ -12,14 +12,14 @@ const about = {
     },
     date: "30 de agosto de 2025",
     location: {
-        adress: "Rua dos Três Irmãos, 123 - Centro, São Paulo - SP",
-        city: "São Paulo",
+        adress: "R. Dr. Eduardo Nielsen, 420 - Jardim Congonhas, São José do Rio Preto - SP",
+        city: "São José do Rio Preto",
         state: "SP",
         country: "Brasil",
-        zip: "12345-678",
-        google_maps_link: "https://maps.app.goo.gl/3hc7kGxsnRmDUfH99",
+        zip: "15030-070",
+        google_maps_link: "https://maps.app.goo.gl/LxUpQV1e3f4kFjqn7",
         google_maps_link_text: "Ver no Google Maps",
-        google_maps_embed_link: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3729.4287804546907!2d-49.39409292392956!3d-20.814384966458203!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94bdad65cc571127%3A0x96b5489c2ccb0900!2sACIRP%20the%20Convention%20Center!5e0!3m2!1sen!2sbr!4v1746703766828!5m2!1sen!2sbr"
+        google_maps_embed_link: "https://maps.google.com/maps?width=600&height=400&hl=en&q=R.%20Dr.%20Eduardo%20Nielsen%2C%20420%20-%20Jardim%20Congonhas&t=&z=14&ie=UTF8&iwloc=B&output=embed"
     }
 }
 
